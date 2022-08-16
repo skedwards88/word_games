f52c1b92854fdef5755876af188bdf969bd4e4b2
skedwards88 <skedwards88@github.com>
2022-08-15 17:33:43 -0700

    change piece creation logic

--- src/jigsaw/logic/gameInit.js
+++ src/jigsaw/logic/gameInit.js
@@ -53,6 +53,7 @@ function trimPiece(piece) {
 }
 
 function getPiecesFromBoard(grid, pieceSize) {
+  // console.log(JSON.stringify(grid))
   // break the board into subgrids
   const partitionedRows = grid.map((row) => partitionArray(row, pieceSize));
   let pieces = [];
@@ -87,6 +88,104 @@ function getPiecesFromBoard(grid, pieceSize) {
   return shuffleArray(pieces);
 }
 
+
+function assemblePiece(pieceData) {
+  const maxTop = pieceData.map(data => data.top).reduce(
+    (currentMax, comparison) =>
+      currentMax > comparison ? currentMax : comparison,
+    0
+  )
+  const minTop = pieceData.map(data => data.top).reduce(
+    (currentMin, comparison) =>
+    currentMin < comparison ? currentMin : comparison,
+    0
+  )
+  const numRows = maxTop - minTop + 1;
+
+  const maxLeft = pieceData.map(data => data.left).reduce(
+    (currentMax, comparison) =>
+      currentMax > comparison ? currentMax : comparison,
+    0
+  )
+  const minLeft = pieceData.map(data => data.left).reduce(
+    (currentMin, comparison) =>
+    currentMin < comparison ? currentMin : comparison,
+    0
+  )
+  const numCols = maxLeft - minLeft + 1;
+
+  let grid = Array.from({ length: numRows }, () =>
+  Array.from({ length: numCols }, () => "")
+);
+for (let pieceIndex = 0; pieceIndex < pieceData.length; pieceIndex++) {
+  grid[pieceData[pieceIndex].top][pieceData[pieceIndex].left] = pieceData[pieceIndex].letter
+  
+}
+return grid
+
+}
+
+
+function makePieces(grid) {
+  const remainingGrid = JSON.parse(JSON.stringify(grid));
+  const piecesData = [];
+
+  for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+    for (let colIndex = 0; colIndex < grid[rowIndex].length; colIndex++) {
+      // at this index, check if there is a letter in the remaining grid
+      if (remainingGrid[rowIndex][colIndex]) {
+        // if there is a letter, remove the letter from the remaining grid and start a piece
+        const letter = remainingGrid[rowIndex][colIndex];
+        let pieceData = [
+          {
+            letter: letter,
+            top: 0,
+            left: 0,
+          }
+        ]
+        remainingGrid[rowIndex][colIndex] = ""
+        // console.log(`there is a letter ${letter} at ${rowIndex},${colIndex}`)
+
+        // from this starting point, look in each direction for a letter
+        // todo adj for outward
+        let pieceLevel = 0;
+        const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]; //todo can randomize order
+        while (pieceData.length < 5 && pieceLevel < pieceData.length) {//todo can randomize num letters
+          // console.log(`START WHILE ${rowIndex}/${colIndex}: pieceLevel: ${pieceLevel}, piece.length: ${pieceData.length}`)
+          let surroundedPieceRow = pieceData[pieceLevel].top;
+          let surroundedPieceCol = pieceData[pieceLevel].left;
+          // console.log(`surroundedPieceRow ${surroundedPieceRow}, surroundedPieceCol ${surroundedPieceCol}`)
+          for (let directionIndex = 0; directionIndex < directions.length; directionIndex++) {
+            const [rowOffset, colOffset] = directions[directionIndex];
+            // console.log(`check row ${rowIndex + surroundedPieceRow + rowOffset} from ${rowIndex}+${surroundedPieceRow}+${rowOffset}/check col ${colIndex + surroundedPieceCol + colOffset} from ${colIndex}+${surroundedPieceCol}+${colOffset}`)
+            // if there is a letter, remove the letter from the remaining grid and add it to the piece
+            if (remainingGrid?.[rowIndex + surroundedPieceRow + rowOffset]?.[colIndex + surroundedPieceCol + colOffset]) {
+              const surroundingLetter = remainingGrid[rowIndex + surroundedPieceRow + rowOffset][colIndex + surroundedPieceCol + colOffset]
+              // console.log(`found a letter ${surroundingLetter}`)
+              pieceData.push({
+                letter: surroundingLetter,
+                top: surroundedPieceRow + rowOffset,
+                left: surroundedPieceCol + colOffset,
+              })
+              remainingGrid[rowIndex + surroundedPieceRow + rowOffset][colIndex + surroundedPieceCol + colOffset] = ""
+            } else {
+              // console.log('no letter here')
+            }
+          }
+          pieceLevel++
+        }
+        piecesData.push(pieceData)
+      }
+      // then look in all directions around the second letter
+      // continue until [3-4] letter in the piece
+      // or until there are no more surrounding pieces
+    }
+
+  }
+  const pieces = piecesData.map(data => assemblePiece(data))
+  return pieces
+}
+
 export function gameInit({ useSaved, sortBy }) {
   // const savedState =
   //   useSaved ?? true
@@ -117,8 +216,9 @@ export function gameInit({ useSaved, sortBy }) {
     maxWordLength: maxWordLength,
   });
 
-  const pieces = getPiecesFromBoard(grid, pieceSize);
-
+  // const pieces = getPiecesFromBoard(grid, pieceSize);
+  const pieces = makePieces(grid);
+  console.log(JSON.stringify(pieces))
   const pieceData = pieces.map((piece, index) => ({
     letters: piece,
     id: index,
