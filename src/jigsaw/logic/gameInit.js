73f8c548e07cdac56104278bfa90a9f3b85d7288
skedwards88 <skedwards88@github.com>
2022-08-16 12:24:27 -0700

    remove old piece generation logic

--- src/jigsaw/logic/gameInit.js
+++ src/jigsaw/logic/gameInit.js
@@ -1,92 +1,5 @@
 import { generateGrid } from "./generateGrid";
-import { partitionArray } from "../../common/partitionArray";
 import { shuffleArray } from "../../common/shuffleArray";
-
-function rowIsEmpty(row) {
-  return !row.filter((letter) => letter).length;
-}
-
-function trimPiece(piece) {
-  let trimmedPiece = JSON.parse(JSON.stringify(piece));
-
-  // trim from top
-  for (let rowIndex = 0; rowIndex < piece.length; rowIndex++) {
-    if (rowIsEmpty(piece[rowIndex])) {
-      trimmedPiece.splice(0, 1);
-    } else {
-      break;
-    }
-  }
-  // trim from bottom
-  for (let rowIndex = piece.length - 1; rowIndex >= 0; rowIndex--) {
-    if (rowIsEmpty(piece[rowIndex])) {
-      trimmedPiece.splice(trimmedPiece.length - 1, 1);
-    } else {
-      break;
-    }
-  }
-  // trim from left
-  for (let colIndex = 0; colIndex < piece[0].length; colIndex++) {
-    const column = piece.map((row) => row[colIndex]);
-    if (rowIsEmpty(column)) {
-      const numRows = trimmedPiece.length;
-      for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
-        trimmedPiece[rowIndex].splice(0, 1);
-      }
-    } else {
-      break;
-    }
-  }
-  // trim from right
-  for (let colIndex = piece.length - 1; colIndex >= 0; colIndex--) {
-    const column = piece.map((row) => row[colIndex]);
-    if (rowIsEmpty(column)) {
-      const numRows = trimmedPiece.length;
-      for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
-        trimmedPiece[rowIndex].splice(trimmedPiece[rowIndex].length - 1, 1);
-      }
-    } else {
-      break;
-    }
-  }
-  return trimmedPiece;
-}
-
-function getPiecesFromBoard(grid, pieceSize) {
-  // console.log(JSON.stringify(grid))
-  // break the board into subgrids
-  const partitionedRows = grid.map((row) => partitionArray(row, pieceSize));
-  let pieces = [];
-  for (
-    let startRow = 0;
-    startRow <= partitionedRows.length - pieceSize;
-    startRow = startRow + pieceSize
-  ) {
-    for (
-      let colIndex = 0;
-      colIndex < partitionedRows[startRow].length;
-      colIndex++
-    ) {
-      let piece = [];
-      for (
-        let rowIndex = startRow;
-        rowIndex < startRow + pieceSize;
-        rowIndex++
-      ) {
-        piece.push(partitionedRows[rowIndex][colIndex]);
-      }
-
-      // add the piece if it is not empty
-      if (piece.filter((row) => row.filter((letter) => letter).length).length) {
-        const trimmedPiece = trimPiece(piece);
-
-        pieces.push(trimmedPiece);
-      }
-    }
-  }
-  // return pieces;
-  return shuffleArray(pieces);
-}
 
 function getPieceDimension(pieceData) {
   const maxTop = pieceData
@@ -254,7 +167,6 @@ export function gameInit({ useSaved, sortBy }) {
   // }
 
   const gridSize = 12;
-  let pieceSize = 3;
   const minLetters = 40;
   const minWordLength = 4;
   const maxWordLength = 7;
@@ -266,8 +178,7 @@ export function gameInit({ useSaved, sortBy }) {
   });
 
   console.log(JSON.stringify(grid));
-  // const pieces = getPiecesFromBoard(grid, pieceSize);
-  const pieces = makePieces(grid);
+  const pieces = shuffleArray(makePieces(grid));
   const pieceData = pieces.map((piece, index) => ({
     letters: piece,
     id: index,
@@ -275,25 +186,6 @@ export function gameInit({ useSaved, sortBy }) {
     boardLeft: undefined,
     poolIndex: index,
   }));
-
-  // let pieceData = []
-  // let currentTop = gridSize - pieceSize;
-  // let currentLeft = gridSize - pieceSize;
-  // for (let pieceIndex = 0; pieceIndex < pieces.length; pieceIndex++) {
-  //   const piece = {
-  //     letters: pieces[pieceIndex],
-  //     id: pieceIndex,
-  //     boardTop: currentTop,
-  //     boardLeft: currentLeft,
-  //   }
-  //   pieceData.push(piece)
-  //   if (currentLeft === 0) {
-  //     currentLeft = gridSize - pieceSize;
-  //     currentTop = currentTop - pieceSize
-  //   } else {
-  //     currentLeft = currentLeft - pieceSize
-  //   }
-  // }
 
   //todo trim off excess whitespace row/col on edge
   return {
