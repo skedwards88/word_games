acc6baa8767c2de4fa0f5ef55fc1cc5a6455752c
skedwards88 <skedwards88@github.com>
2023-01-01 13:16:12 -0800

    update processors to exclude offensive words

--- src/common/wordLists/processed/gutenberg_processor.py
+++ src/common/wordLists/processed/gutenberg_processor.py
@@ -13,8 +13,15 @@ with open(f"{root}raw/gutenberg.txt", "r") as inFile:
       continue
     gutenberg.append(word.upper())
 
+offensive = []
+with open(f"{root}raw/LDNOOBW.txt", "r") as file:
+  for line in file:
+    offensive.append(line.strip().upper())
+
 common = list(set(gutenberg).intersection(set(wordnik)))
+commonMinusOffensive = list(set(common).difference(set(offensive)))
+commonMinusOffensive.sort()
 
 with open(f"{root}processed/gutenberg.txt", "w") as file:
-  for word in common:
+  for word in commonMinusOffensive:
     file.writelines(f"{word}\n")
--- src/common/wordLists/processed/wiki_processor.py
+++ src/common/wordLists/processed/wiki_processor.py
@@ -15,8 +15,15 @@ with open(f"{root}raw/wiki.txt", "r") as inFile:
       break
     wiki.append(word.upper())
 
+offensive = []
+with open(f"{root}raw/LDNOOBW.txt", "r") as file:
+  for line in file:
+    offensive.append(line.strip().upper())
+    
 common = list(set(wiki).intersection(set(wordnik)))
+commonMinusOffensive = list(set(common).difference(set(offensive)))
+commonMinusOffensive.sort()
 
 with open(f"{root}processed/wiki.txt", "w") as file:
-  for word in common:
+  for word in commonMinusOffensive:
     file.writelines(f"{word}\n")
